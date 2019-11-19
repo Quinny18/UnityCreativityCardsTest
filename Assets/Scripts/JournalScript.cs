ebe2b5022663c094db37216fab2ad0aa36beb96b
Quinny18 <56097996+Quinny18@users.noreply.github.com>
2019-11-19 00:38:32 +0000

    Initial Commit Added Assets

---
+++ Assets/Scripts/JournalScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalScript : MonoBehaviour
+{
+    /*Crab
+    public CrabInteraction()
+    {
+        //if crab is clicked on and successfully interrogated then crab page overlays blank page
+        //Crab overlay covers half of BGV3
+        Debug.Log("Crab");
+
+
+    }
+
+    //Fish
+    public FishInteraction()
+    {
+        Debug.Log("Fish");
+    }
+    //Jellyfish
+    public JellyfishInteraction()
+    {
+        Debug.Log("Jellyfish");
+    }
+    //MorayEel
+    public MorayInteraction()
+    {
+        Debug.Log("Moray");
+    }
+    //Seahorse
+    public SeahorseInteraction()
+    {
+        Debug.Log("Seahorse");
+    }
+    //Seaweed
+    public SeaweedInteraction()
+    {
+        Debug.Log("Seaweed");
+    }
+    //Shark
+    public SharkInteraction()
+    {
+        Debug.Log("Shark");
+    }
+    //Squid
+    public SquidInteraction()
+    {
+        Debug.Log("Squid");
+      
+    }
+    */
+}
+
